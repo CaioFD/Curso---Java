9f984344b0968f6133ac445c874a2fa33f00abdb
CaioFD <caiodiniz200204@gmail.com>
2024-08-23 14:31:41 -0300

    modified:   Exercicios/src/Fundamentos/PrimeiroPrograma.java

--- Exercicios/src/Fundamentos/PrimeiroPrograma.java
+++ Exercicios/src/Fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,7 @@ package Fundamentos;
 public class PrimeiroPrograma{
 
     public static void main(String[] args){
-        System.out.println("Primeiro Programa!!");
+        System.out.println("Primeiro Programa parte 1!!");
+        System.out.println("Primeiro Programa parte 2!!");
     }
 }
